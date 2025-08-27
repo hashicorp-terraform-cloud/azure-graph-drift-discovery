1b6f5bf1722c56b82cd751c1ae704429fa583df3
Ben Holmes <ben.holmes@hashicorp.com>
2025-08-27 21:38:47 +0100

    removed tier-gated permissions

--- azuread.tf
+++ azuread.tf
@@ -1,11 +1,10 @@
 // create a group and user with the azuread provider
 resource "azuread_group" "group" {
-  display_name       = var.group_base_name
-  description        = "Managed by Terraform - AzureAD Provider"
-  mail_enabled       = false
-  mail_nickname      = "azuread"
-  security_enabled   = true
-  assignable_to_role = true
+  display_name     = var.group_base_name
+  description      = "Managed by Terraform - AzureAD Provider"
+  mail_enabled     = false
+  mail_nickname    = "azuread"
+  security_enabled = true
 }
 
 resource "random_password" "password" {
@@ -25,23 +24,3 @@ resource "azuread_group_member" "member" {
   group_object_id  = azuread_group.group.object_id
   member_object_id = azuread_user.user.object_id
 }
-
-// assign the group to the built-in Reader role
-resource "azuread_directory_role" "role" {
-  display_name = "Global Reader"
-}
-
-resource "azuread_directory_role_assignment" "assignment" {
-  role_id             = azuread_directory_role.role.template_id
-  principal_object_id = azuread_group.group.object_id
-}
-
-# // lets invite a b2b user to the directory
-# resource "azuread_invitation" "b2b" {
-#   user_email_address = var.b2b_email
-#   redirect_url       = var.b2b_invite_redirect_url
-
-#   message {
-#     body = "Hello there! You are invited to join the ${var.domain_name} Azure tenant!"
-#   }
-# }
