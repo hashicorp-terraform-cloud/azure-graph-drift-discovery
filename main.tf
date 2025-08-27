c141e148ca2e2d19155f34aafde3e0bba86ed99f
Ben Holmes <ben.holmes@hashicorp.com>
2025-08-27 15:31:19 +0100

    added data and output

--- main.tf
+++ main.tf
@@ -19,3 +19,15 @@ resource "msgraph_resource" "group" {
     securityEnabled = true
   }
 }
+
+data "msgraph_resource" "group" {
+  url = "groups/${msgraph_resource.group.id}"
+  response_export_values = {
+    all          = "@"
+    display_name = "displayName"
+  }
+}
+
+output "all" {
+  value = data.msgraph_resource.group.output.all
+}
