0c988ace677ae856b2da367d65ba30f1c122a9e8
Ben Holmes <ben.holmes@hashicorp.com>
2025-09-12 16:29:40 +0100

    added a seond assertion to check explicit memberships

--- terraform/validation.tf
+++ terraform/validation.tf
@@ -1,4 +1,4 @@
-check "check_group_memberships" {
+check "verify_group_membership" {
   data "azuread_group" "managed_group" {
     display_name = var.group_base_name
   }
@@ -6,5 +6,13 @@ check "check_group_memberships" {
   assert {
     condition     = length(data.azuread_group.managed_group.members) == length(azuread_user.user)
     error_message = "${length(data.azuread_group.managed_group.members)} users exist in the group, but ${length(azuread_user.user)} users are managed by Terraform"
+  }
+
+  assert {
+    condition = alltrue([
+      for user_key, user in azuread_user.user :
+      contains(data.azuread_group.managed_group.members, user.object_id)
+    ])
+    error_message = "Not all Terraform-managed users are present in the group membership list"
   }
 }
