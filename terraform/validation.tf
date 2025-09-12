2e4f86e894482057cff397bcc6fd28daa6787061
Ben Holmes <ben.holmes@hashicorp.com>
2025-09-12 16:23:26 +0100

    removed total user check

--- terraform/validation.tf
+++ terraform/validation.tf
@@ -1,12 +1,3 @@
-check "check_managed_users" {
-  data "azuread_users" "all_users" { return_all = true }
-
-  assert {
-    condition     = length(data.azuread_users.all_users.user_principal_names) == length(azuread_user.user)
-    error_message = "${length(data.azuread_users.all_users.user_principal_names)} users exist, but ${length(azuread_user.user)} users are managed by Terraform"
-  }
-}
-
 check "check_group_memberships" {
   data "azuread_group" "managed_group" {
     display_name = var.group_base_name
