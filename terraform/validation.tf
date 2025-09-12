a27d74364e8a20dde6c8e8ad9fb611c89bdcf46d
Ben Holmes <ben.holmes@hashicorp.com>
2025-09-12 16:07:54 +0100

    syntax error

--- terraform/validation.tf
+++ terraform/validation.tf
@@ -8,12 +8,12 @@ check "check_managed_users" {
 }
 
 check "check_group_memberships" {
-  data "azure_ad_group" "managed_group" {
+  data "azuread_group" "managed_group" {
     display_name = var.group_base_name
   }
 
   assert {
-    condition     = length(data.azure_ad_group.managed_group.members) == length(azuread_user.user)
-    error_message = "${length(data.azure_ad_group.managed_group.members)} users exist in the group, but ${length(azuread_user.user)} users are managed by Terraform"
+    condition     = length(data.azuread_group.managed_group.members) == length(azuread_user.user)
+    error_message = "${length(data.azuread_group.managed_group.members)} users exist in the group, but ${length(azuread_user.user)} users are managed by Terraform"
   }
 }
